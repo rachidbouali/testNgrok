289064f1a066068ffa1d1fb20a10188af68adef3
RB <RB@LAPTOP-8EB5BJOK>
2020-07-15 14:56:07 -0400

    commit2

--- src/main/AppCtr.java
+++ src/main/AppCtr.java
@@ -17,6 +17,12 @@ public class AppCtr {
     public static void main(String[] args) {
         
         System.out.println("Bonjour "+args[1]+", " +args[0]);
+        
+        System.out.println("Au revoir!");
+        
+        System.out.println("Astlavista!");
+        
+        System.out.println("Merci!");
     }
     
 }
